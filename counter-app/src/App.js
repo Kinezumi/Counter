c28298676d0955418423948719065c89aef74fc5
Kinezumi <kinezumi91@gmail.com>
2017-08-30 14:07:36 +0300

    first commit

---
+++ counter-app/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    state = {value: 0};
+    
+    increment = () => {
+        this.setState({value: this.state.value + 1});
+    }
+    
+    decrement = () => {
+        this.setState({value: this.state.value - 1});
+    }
+    
+    reset = () => {
+        this.setState({value: this.state.value = 0});
+    }
+    
+    render(){
+        return(
+            <div>
+                <div>{this.state.value}</div>
+                <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement}>-</button>
+                <button onClick={this.reset}>reset</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
